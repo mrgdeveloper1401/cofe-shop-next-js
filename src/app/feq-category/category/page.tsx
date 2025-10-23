be62487ebf0c51107cef1a0d81858098a0ef1e0a
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-10-23 15:25:49 +0000

    add page category

---
+++ src/app/feq-category/category/page.tsx
@@ -0,0 +1,7 @@
+import QuestionCategory from "@/components/feq_category/QuestionCategoryies";
+
+export default function AppQuestionCategory(){
+    return (
+        <QuestionCategory/>
+    )
+}
---
+++ src/components/feq_category/QuestionCategoryies.tsx
@@ -0,0 +1,153 @@
+export default function QuestionCategory(){
+    return (
+        <main className="main-content dt-sl mb-3" style={{ marginTop: '204.157px' }}>
+            <div className="container main-container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="page info-page-cats dt-sl dt-sn pt-3 pb-2">
+                            <div className="row">
+                                <div className="col-12 pr-4 mb-3">
+                                    <div className="section-title title-wide mb-1 no-after-title-wide">
+                                        <h2 className="font-weight-bold px-res-2">دسته‌بندی پرسش‌ها</h2>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4 col-6">
+                                    <div className="info-page-cat">
+                                        <a href="#">
+                                            <div className="info-page-cat-icon">
+                                                <img src="/assets/img/faq/1.png" alt=""/>
+                                            </div>
+                                            <span className="info-page-cat-title">
+                                                ورود و ثبت نام
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4 col-6">
+                                    <div className="info-page-cat">
+                                        <a href="#">
+                                            <div className="info-page-cat-icon">
+                                                <img src="/assets/img/faq/2.png" alt=""/>
+                                            </div>
+                                            <span className="info-page-cat-title">
+                                                روند ثبت سفارش
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4 col-6">
+                                    <div className="info-page-cat">
+                                        <a href="#">
+                                            <div className="info-page-cat-icon">
+                                                <img src="/assets/img/faq/3.png" alt=""/>
+                                            </div>
+                                            <span className="info-page-cat-title">
+                                                پیگیری ارسال سفارش
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4 col-6">
+                                    <div className="info-page-cat">
+                                        <a href="#">
+                                            <div className="info-page-cat-icon">
+                                                <img src="/assets/img/faq/4.png" alt=""/>
+                                            </div>
+                                            <span className="info-page-cat-title">
+                                                بازگرداندن کالا
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4 col-6">
+                                    <div className="info-page-cat">
+                                        <a href="#">
+                                            <div className="info-page-cat-icon">
+                                                <img src="/assets/img/faq/5.png" alt=""/>
+                                            </div>
+                                            <span className="info-page-cat-title">
+                                                کد تخفیف و کارت هدیه
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4 col-6">
+                                    <div className="info-page-cat">
+                                        <a href="#">
+                                            <div className="info-page-cat-icon">
+                                                <img src="/assets/img/faq/6.png" alt=""/>
+                                            </div>
+                                            <span className="info-page-cat-title">
+                                                سوپرمارکت
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4 col-6">
+                                    <div className="info-page-cat">
+                                        <a href="#">
+                                            <div className="info-page-cat-icon">
+                                                <img src="/assets/img/faq/7.png" alt=""/>
+                                            </div>
+                                            <span className="info-page-cat-title">
+                                                تعهد به شفافیت
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4 col-6">
+                                    <div className="info-page-cat">
+                                        <a href="#">
+                                            <div className="info-page-cat-icon">
+                                                <img src="/assets/img/faq/8.png" alt=""/>
+                                            </div>
+                                            <span className="info-page-cat-title">
+                                                چند مرسوله ایی
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4 col-6">
+                                    <div className="info-page-cat">
+                                        <a href="#">
+                                            <div className="info-page-cat-icon">
+                                                <img src="/assets/img/faq/9.png" alt=""/>
+                                            </div>
+                                            <span className="info-page-cat-title">
+                                                دیجی پی
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4 col-6">
+                                    <div className="info-page-cat">
+                                        <a href="#">
+                                            <div className="info-page-cat-icon">
+                                                <img src="/assets/img/faq/10.png" alt=""/>
+                                            </div>
+                                            <span className="info-page-cat-title">
+                                                دیجی کلاپ
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4 col-6">
+                                    <div className="info-page-cat">
+                                        <a href="#">
+                                            <div className="info-page-cat-icon">
+                                                <img src="/assets/img/faq/11.png" alt=""/>
+                                            </div>
+                                            <span className="info-page-cat-title">
+                                                سایر موارد
+                                            </span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
